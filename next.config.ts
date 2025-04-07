5d94b64065b41551f787d1b10022545e5fcaeb60
Hannan Syed Shah <hannansshah2004@gmail.com>
2025-04-07 17:46:52 -0400

    ignore stagehand-service

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': ['./stagehand-service/**/*'],
+    },
+  },
 };
 
 export default nextConfig;
